331bc256a421b8809442b227784344d43582948c
Derrick441 <1412652558@qq.com>
2020-08-17 10:39:22 +0800

    perf functions : IC

---
+++ codes/performance/perf_functions.py
@@ -0,0 +1,37 @@
+def perf_evaluation_IC_raw(ret, alpha, method):
+    """
+    calculate IC or rank IC with raw data, and the data need Groupby
+    :param ret: return of stocks
+    :param alpha: alpha factor
+    :param method: pearson for IC and spearman for rankIC
+    :return:
+    """
+    f = alpha.to_frame('alpha')
+    f['ret'] = ret
+    ic = f.groupby(level=[0, 1]).apply(perf_single_IC, method=method)
+    return ic
+
+
+def perf_evaluation_IC(f, method):
+    """
+    calculate IC or rank IC with concat data, and the data need Groupby
+    :param f: column1 is return of stocks, column2 is alpha
+    :param method: pearson for IC and spearman for rankIC
+    :return:
+    """
+    ic = f.groupby(level=[0, 1]).apply(perf_single_IC, method=method)
+    return ic
+
+
+def perf_single_IC(f, method):
+    """
+    calculate IC or rank IC with concat data
+    :param f: column1 is return of stocks, column2 is alpha
+    :param method: pearson for IC and spearman for rankIC
+    :return:
+    """
+    if method == 'IC':
+        ic = f.corr(method='pearson', mini_periods=1)
+    else:  # for rankIC
+        ic = f.corr(method='spearman', mini_periods=1)
+    return ic
