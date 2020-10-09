107e65da1d5bd8bb66886168ca09bc6ab55fec6d
Derrick441 <1412652558@qq.com>
2020-10-09 14:40:07 +0800

    rewrite fama-french's 3 factor at 5min level(3factor,2code)

--- codes/factor/mktfactor/hq_factor_mkt.py
+++ codes/factor/mktfactor/hq_factor_mkt.py
@@ -6,29 +6,28 @@ import time
 # 市场因子：中证500收益率
 class Mkt(object):
 
-    def __init__(self, indir, index):
-        self.indir = indir
-        self.index = index
+    def __init__(self, file_indir, save_indir, file_name):
+        self.file_indir = file_indir
+        self.save_indir = save_indir
+        self.file_name = file_name
 
     def filein(self):
         t = time.time()
-        self.mkt_data_row = pd.read_pickle(self.indir + self.index)
+        self.mkt_data_1min = pd.read_pickle(self.file_indir + self.file_name)
         print('filein running time:%10.4fs' % (time.time() - t))
 
-    def compose_one_five(self, data):
-        # 显示
+    def compose_five(self, data):
         print(data.iloc[0, 0]+'-'+data.iloc[0, 2])
-        # 数据排序（9.30->15.00）
+        # 数据排序
         data1 = data.sort_values(by='bargaintime')
         # 索引
-        len1 = len(data)
-        len2 = len1-1
-        head_list = list(range(0, len2, 5))
-        tail_list = list(range(5, len2, 5))
-        tail_list.append(len1)
-        # 合并数据存放空间
+        num1 = len(data)
+        num2 = num1-1
+        head_list = list(range(0, num2, 5))
+        tail_list = list(range(5, num2, 5))
+        tail_list.append(num1)
+        # 1分钟数据整合为5分钟数据
         result_list = []
-        # 合并
         for i, j in zip(head_list, tail_list):
             info_list = []
             temp_data = data1.values[i:j, :]
@@ -43,53 +42,50 @@ class Mkt(object):
             info_list.append(temp_data[:, 8].sum())
             info_list.append(temp_data[:, 9].sum())
             result_list.append(info_list)
-        # 格式调整
+        # 整理
         result = pd.DataFrame(result_list)
         result.columns = data.columns
-        # 返回合并结果
         return result
 
     def datamanage(self):
         t = time.time()
         # 一分钟数据合并成5分钟数据
-        self.mkt_data = self.mkt_data_row.groupby(['s_info_windcode', 'trade_dt'])\
-                                         .apply(self.compose_one_five)\
-                                         .reset_index(drop=True)
+        self.mkt_data = self.mkt_data_1min.groupby(['s_info_windcode', 'trade_dt'])\
+                                          .apply(self.compose_five)\
+                                          .reset_index(drop=True)
         print('compose running time:%10.4fs' % (time.time() - t))
 
-    def mkt(self):
+    def compute(self):
         t = time.time()
         # 每5分钟市场因子
         self.mkt_data['mkt'] = (self.mkt_data.closeprice - self.mkt_data.openprice) / self.mkt_data.openprice * 100
-        # 结果
-        item = ['trade_dt', 'bargaintime', 'mkt']
-        self.result = self.mkt_data[item]
         print('mkt running time:%10.4fs' % (time.time() - t))
 
     def fileout(self):
         t = time.time()
-        # 存在factor文件夹的basicfactor中
-        indir_factor = 'D:\\wuyq02\\develop\\python\\data\\factor\\mktfactor\\'
-        self.result.to_pickle(indir_factor + 'factor_mkt_5min_' + self.index[19:23] + '.pkl')
+        # 数据输出
+        item = ['trade_dt', 'bargaintime', 'mkt']
+        self.mkt_data[item].to_pickle(self.save_indir + 'factor_mkt_5min_' + self.file_name[19:23] + '.pkl')
         print('fileout running time:%10.4fs' % (time.time() - t))
 
     def runflow(self):
-        print('compute start')
+        print('start')
         t = time.time()
         self.filein()
         self.datamanage()
-        self.mkt()
+        self.compute()
         self.fileout()
-        print('compute end, running time:%10.4f' % (time.time()-t))
+        print('finish using time:%10.4f' % (time.time()-t))
         return self
 
 
 if __name__ == '__main__':
     file_indir = 'D:\\wuyq02\\develop\\python\\data\\developflow\\zz500\\'
-    file_index = ['zz500_store_hqdata_2012.pkl', 'zz500_store_hqdata_2013.pkl', 'zz500_store_hqdata_2014.pkl',
+    save_indir = 'D:\\wuyq02\\develop\\python\\data\\factor\\mktfactor\\'
+    file_names = ['zz500_store_hqdata_2012.pkl', 'zz500_store_hqdata_2013.pkl', 'zz500_store_hqdata_2014.pkl',
                   'zz500_store_hqdata_2015.pkl', 'zz500_store_hqdata_2016.pkl', 'zz500_store_hqdata_2017.pkl',
                   'zz500_store_hqdata_2018.pkl', 'zz500_store_hqdata_2019.pkl']
-    for i in file_index:
-        print(i[19:23])
-        mkt = Mkt(file_indir, i)
+    for file_name in file_names:
+        print(file_name)
+        mkt = Mkt(file_indir, save_indir, file_name)
         mkt.runflow()
---
+++ codes/factor/mktfactor/hq_factor_smb_hml.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import time
+
+
+# 5分钟频率
+# 市值因子：每日，根据股票市值，取市值前1/3股票作小市值组合，取市值后1/3股票作大市值组合，两组股票收益率均值之差作市值因子
+class TwoFactor(object):
+
+    def __init__(self, file_indir, save_indir, file_name):
+        self.file_indir = file_indir
+        self.save_indir = save_indir
+        self.file_name = file_name
+
+    def filein(self):
+        t = time.time()
+        self.all_data = pd.read_pickle(self.file_indir + self.file_name)
+        print('filein running time:%10.4fs' % (time.time() - t))
+
+    def datamanage(self):
+        pass
+
+    def minute_factor(self, data, index):
+        num = int(len(data) / 3)
+        # 取流通市值/估值最小1/3股票的收益率均值
+        low = data.sort_values(by=index).iloc[:num, :].change.mean()
+        # 取流通市值/估值最大1/3股票的收益率均值
+        high = data.sort_values(by=index).iloc[-num:, :].change.mean()
+        # 市值因子
+        return low - high
+
+    def compute(self):
+        t = time.time()
+        # 每5分钟市值因子
+        self.result1 = self.all_data.groupby(['trade_dt', 'bargaintime'])\
+                                    .apply(self.minute_factor, 'mv')\
+                                    .reset_index()\
+                                    .rename(columns={0: 'smb'})
+        # 每5分钟估值因子
+        self.result2 = self.all_data.groupby(['trade_dt', 'bargaintime'])\
+                                    .apply(self.minute_factor, 'pe')\
+                                    .reset_index()\
+                                    .rename(columns={0: 'hml'})
+        print('compute running time:%10.4fs' % (time.time() - t))
+
+    def fileout(self):
+        t = time.time()
+        # 数据输出
+        self.result1.to_pickle(self.save_indir + 'factor_smb_5min_' + self.file_name[17:21] + '.pkl')
+        self.result2.to_pickle(self.save_indir + 'factor_hml_5min_' + self.file_name[17:21] + '.pkl')
+        print('fileout running time:%10.4fs' % (time.time() - t))
+
+    def runflow(self):
+        print('start')
+        t = time.time()
+        self.filein()
+        self.datamanage()
+        self.compute()
+        self.fileout()
+        print('finish using time:%10.4f' % (time.time()-t))
+        return self
+
+
+if __name__ == '__main__':
+    file_indir = 'D:\\wuyq02\\develop\\python\\data\\developflow\\all\\'
+    save_indir = 'D:\\wuyq02\\develop\\python\\data\\factor\\mktfactor\\'
+    file_names = ['all_store_hqdata_2012_5_derive.pkl', 'all_store_hqdata_2013_5_derive.pkl',
+                  'all_store_hqdata_2014_5_derive.pkl', 'all_store_hqdata_2015_5_derive.pkl',
+                  'all_store_hqdata_2016_5_derive.pkl', 'all_store_hqdata_2017_5_derive.pkl',
+                  'all_store_hqdata_2018_5_derive.pkl', 'all_store_hqdata_2019_5_derive.pkl']
+    for file_name in file_names:
+        print(file_name)
+        tf = TwoFactor(file_indir, save_indir, file_name)
+        tf.runflow()
